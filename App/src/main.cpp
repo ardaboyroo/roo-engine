9f1906251eb169a9d22b2df06168633613dd9e04
ardaboyroo <ardacelebci2005@gmail.com>
2024-11-19 21:46:29 +0100

    Added base project structure
    Added build generator: CMake
    Added script to automate project generation

---
+++ App/src/main.cpp
@@ -0,0 +1,11 @@
+#include "engine.hpp"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello from App!" << std::endl;
+
+    HelloWorld();
+
+    std::cin.get();
+}
---
+++ roo-engine/src/engine.cpp
@@ -0,0 +1,7 @@
+#include "engine.hpp"
+#include <iostream>
+
+void HelloWorld()
+{
+    std::cout << "Hello from roo-engine!" << std::endl;
+}
