63facef37a03f2da5dc38575547588a743bb4472
ardaboyroo <ardacelebci2005@gmail.com>
2025-01-18 15:49:39 +0100

    Made a demo game (Pong)

--- App/src/main.cpp
+++ App/src/main.cpp
@@ -2,11 +2,14 @@
 
 #include <memory>
 
-class TestLayer : public roo::Layer
+// IMPORTANT! : To run this demo make sure the assets folder located in root/App/assets
+// is copied to the according build directory (e.g. root/build/bin/App/Debug/assets)
+
+class PongLayer : public roo::Layer
 {
 public:
-    TestLayer()
-        : Layer("TestLayer"), m_CameraPosition({ 0.0f, 0.0f, 0.0f })
+    PongLayer()
+        : Layer("PongLayer"), window(roo::Application::Get().GetWindow())
     {
         // Heaps and globals are not shared across DLL boundaries! 
         // You will need to call SetCurrentContext() + SetAllocatorFunctions()
@@ -15,205 +18,140 @@ public:
 
     void OnAttach()
     {
-        m_VertexArray = std::make_shared<roo::VertexArray>();
+        paddle1 = std::make_shared<roo::Rectangle>(glm::vec2(20, 100), glm::vec2(10, 100), glm::vec3(.8, .1, .3));
+        AddGameObject(paddle1);
 
-        float vertices[3 * 5] = {
-            -0.5, -0.5f, 0.0f, 1.0f, 1.0f,
-            0.5f, -0.5f, 0.0f, 0.0f, 0.0f,
-            0.0f, 0.5f, 0.0f, 1.0f, 0.0f,
-        };
+        paddle2 = std::make_shared<roo::Rectangle>(glm::vec2(20, 100), 
+            glm::vec2(window.GetWidth() - 10, 100), glm::vec3(.3, .1, .8));
+        AddGameObject(paddle2);
 
-        std::shared_ptr<roo::VertexBuffer> vertexBuffer(new roo::VertexBuffer(vertices, sizeof(vertices)));
-
-
-        roo::BufferLayout layout = {
-            { roo::ShaderDataType::Float3, "VertexPos" },
-            { roo::ShaderDataType::Float2, "color"}
-        };
-
-        vertexBuffer->SetLayout(layout);
-        m_VertexArray->AddVertexBuffer(vertexBuffer);
-
-
-        unsigned int indices[3] = { 0, 1, 2 };
-        std::shared_ptr<roo::IndexBuffer> indexBuffer(new roo::IndexBuffer(indices, sizeof(indices) / sizeof(uint32_t)));
-        m_VertexArray->SetIndexBuffer(indexBuffer);
-
-        std::string vertexSrc = R"(
-#version 330 core
-
-layout(location = 0) in vec3 VertexPos;
-layout(location = 1) in vec2 clr;
-
-uniform mat4 u_ViewProjection;
-
-out vec2 vClr;
-
-void main()
-{
-    vClr = clr;
-    gl_Position = u_ViewProjection * vec4(VertexPos, 1.0f);
-}
-        )";
-
-        std::string fragmentSrc = R"(
-#version 330 core
-
-layout(location = 0) out vec4 color;
-
-in vec2 vClr;
-
-void main()
-{
-    color = vec4(vClr, 0.5f, 1.0f);
-}
-        )";
-
-        m_Shader.reset(new roo::Shader(vertexSrc, fragmentSrc));
-
-        ///////////////////////////////
-        //BACKGROUND                ///
-        ///////////////////////////////
-
-        m_BackgroundVertexArray = std::make_shared<roo::VertexArray>();
-
-        float backgroundVertices[] = {
-            -0.75,  -0.75f, 0.0f, 0.0f, 0.0f,
-             0.75f, -0.75f, 0.0f, 1.0f, 0.0f,
-             0.75f,  0.75f, 0.0f, 1.0f, 1.0f,
-            -0.75f,  0.75f, 0.0f, 0.0f, 1.0f
-        };
-
-        std::shared_ptr<roo::VertexBuffer> backgroundVertexBuffer(
-            new roo::VertexBuffer(backgroundVertices, sizeof(backgroundVertices)));
-
-
-        roo::BufferLayout backgroundLayout = {
-            { roo::ShaderDataType::Float3, "VertexPos" },
-            { roo::ShaderDataType::Float2, "TextureCoordinate"}
-        };
-
-        backgroundVertexBuffer->SetLayout(backgroundLayout);
-        m_BackgroundVertexArray->AddVertexBuffer(backgroundVertexBuffer);
-
-
-        unsigned int backgroundIndices[] = { 0, 1, 2 , 0, 2, 3 };
-        std::shared_ptr<roo::IndexBuffer> backgroundIndexBuffer(new roo::IndexBuffer(backgroundIndices, sizeof(backgroundIndices) / sizeof(uint32_t)));
-        m_BackgroundVertexArray->SetIndexBuffer(backgroundIndexBuffer);
-
-        std::string backgroundVertexSrc = R"(
-#version 330 core
-
-layout(location = 0) in vec3 VertexPos;
-layout(location = 1) in vec2 TexCoord;
-
-uniform mat4 u_ViewProjection;
-
-out vec2 texCoord;
-
-void main()
-{
-    texCoord = TexCoord;
-    gl_Position = u_ViewProjection * vec4(VertexPos, 1.0f);
-}
-        )";
-
-        std::string backgroundFragmentSrc = R"(
-#version 330 core
-
-layout(location = 0) out vec4 color;
-
-in vec2 texCoord;
-
-uniform sampler2D u_Texture;
-
-void main()
-{
-    color = texture(u_Texture, texCoord);
-}
-        )";
-
-        m_BackgroundShader.reset(new roo::Shader(backgroundVertexSrc, backgroundFragmentSrc));
-
-        // Make sure to copy the assets folder from App to build/bin/App/(Debug or Release)/
-        m_Texture.reset(new roo::Texture("assets/Checkerboard.png"));
-        m_TransparentTexture.reset(new roo::Texture("assets/OpenGLlogo.png"));
-        m_BackgroundShader->Bind();
-        m_BackgroundShader->SetUniformInt("u_Texture", 0);
+        ball = std::make_shared<roo::Sprite>("assets/ball.png");
+        ball->SetPosition({ window.GetWidth() / 2.f, window.GetHeight() / 2.f});
+        ball->SetSize({32, 32});
+        AddGameObject(ball);
     }
 
     void OnUpdate(float deltaTime) override
     {
-        if (roo::Input::IsKeyPressed(roo::Key::Left))
-            m_CameraPosition.x -= m_CameraMoveSpeed * deltaTime;
-        else if (roo::Input::IsKeyPressed(roo::Key::Right))
-            m_CameraPosition.x += m_CameraMoveSpeed * deltaTime;
-        if (roo::Input::IsKeyPressed(roo::Key::Up))
-            m_CameraPosition.y += m_CameraMoveSpeed * deltaTime;
-        else if (roo::Input::IsKeyPressed(roo::Key::Down))
-            m_CameraPosition.y -= m_CameraMoveSpeed * deltaTime;
+        // Paddle 1 movement
+        {
+            glm::vec2 pos = paddle1->GetPosition();
+            if (roo::Input::IsKeyPressed(roo::Key::W))
+                pos.y = glm::clamp(pos.y - paddleSpeed, 50.f, window.GetHeight() - 50.f);
 
-        roo::Camera& cam = roo::Application::Get().GetCamera();
+            if (roo::Input::IsKeyPressed(roo::Key::S))
+                pos.y = glm::clamp(pos.y + paddleSpeed, 50.f, window.GetHeight() - 50.f);
 
-        cam.SetPosition(m_CameraPosition);
+            paddle1->SetPosition(pos);
+        }
 
-        roo::Renderer::BeginScene();
+        // Paddle 2 movement
+        {
+            glm::vec2 pos = paddle2->GetPosition();
+            if (roo::Input::IsKeyPressed(roo::Key::Up))
+                pos.y = glm::clamp(pos.y - paddleSpeed, 50.f, window.GetHeight() - 50.f);
 
-        m_Texture->Bind();
-        roo::Renderer::Draw(m_BackgroundShader, m_BackgroundVertexArray);
-        m_TransparentTexture->Bind();
-        roo::Renderer::Draw(m_BackgroundShader, m_BackgroundVertexArray);
-        roo::Renderer::Draw(m_Shader, m_VertexArray);
+            if (roo::Input::IsKeyPressed(roo::Key::Down))
+                pos.y = glm::clamp(pos.y + paddleSpeed, 50.f, window.GetHeight() - 50.f);
 
-        roo::Renderer::EndScene();
+            paddle2->SetPosition(pos);
+        }
+
+        // Paddle 1 Ball collision
+        if (ball->GetPosition().x <= 29.f)
+            if (glm::abs(paddle1->GetPosition().y - ball->GetPosition().y) <= 50.f)
+            {
+                ballVelocity.x *= -1.1;
+                roo::Audio::Play("assets/Bounce.wav");
+            }
+        
+        // Paddle 2 Ball collision
+        if (ball->GetPosition().x >= window.GetWidth() - 29.f)
+            if (glm::abs(paddle2->GetPosition().y - ball->GetPosition().y) <= 50.f)
+            {
+                ballVelocity.x *= -1.1;
+                roo::Audio::Play("assets/Bounce.wav");
+            }
+
+        // Ball bounds collision
+        if (ball->GetPosition().y <= 16.f || ball->GetPosition().y >= window.GetHeight() - 16.f)
+            ballVelocity.y *= -1.f;
+
+        // Ball rotation
+        ball->SetRotation(ball->GetRotation() + 3.f);
+
+        // Ball reset and score
+        if (ball->GetPosition().x <= -16.f || ball->GetPosition().x >= window.GetWidth() + 16.f)
+        {
+            if (ball->GetPosition().x <= -16.f)
+                scoreP2++;
+            else
+                scoreP1++;
+
+            ballVelocity = { -5.f, 1.f};
+            ball->SetPosition({ window.GetWidth() / 2.f, window.GetHeight() / 2.f });
+
+            roo::Audio::Play("assets/Win.wav");
+        }
+
+        // Ball position update
+        ball->SetPosition(ball->GetPosition() + ballVelocity);
     }
 
     void OnEvent(roo::Event& event) override
     {
-        if (event.GetEventType() == roo::EventType::KeyPressed)
-        {
-            roo::KeyPressedEvent& e = (roo::KeyPressedEvent&)event;
-            std::string c;
-            c += e.GetKeyCode();
-            //LOG_INFO(c);
-        }
     }
 
     void OnImGuiRender() override
     {
+        // Create an ImGui window called Debug
         ImGui::Begin("Debug");
+
+        // Add the framerate as a text
         std::string s = "Framerate: ";
         s += S(ImGui::GetIO().Framerate);
         ImGui::Text(s.c_str());
+
+        // Display the scores
+        ImGui::Text(("Red Score: " + S(scoreP1)).c_str());
+        ImGui::Text(("Blue Score: " + S(scoreP2)).c_str());
         ImGui::End();
     }
 
 private:
-    bool isVSync = true;
+    std::shared_ptr<roo::Rectangle> paddle1;
+    std::shared_ptr<roo::Rectangle> paddle2;
 
-    std::shared_ptr<roo::Shader> m_Shader;
-    std::shared_ptr<roo::VertexArray> m_VertexArray;
+    std::shared_ptr<roo::Sprite> ball;
+    glm::vec2 ballVelocity = {-5.0f, 1.0f};
 
-    std::shared_ptr<roo::Shader> m_BackgroundShader;
-    std::shared_ptr<roo::VertexArray> m_BackgroundVertexArray;
+    const float paddleSpeed = 5.0f;
 
-    std::shared_ptr<roo::Texture> m_Texture, m_TransparentTexture;
+    int scoreP1 = 0;
+    int scoreP2 = 0;
 
-    glm::vec3 m_CameraPosition;
-    float m_CameraMoveSpeed = 5.0f;
+    roo::Window& window;
 };
 
 class MyApp : public roo::Application
 {
 public:
     MyApp()
+        // Initialize the application
+        : Application(roo::WindowSettings(
+            "Pong!",
+            800,
+            600,
+            true)
+        )
     {
-        PushLayer(new TestLayer());
-        SetBackgroundColor(0.2f, 0.25f, 0.3f);
+        // Create and push a new layer, this will be the main working environment
+        PushLayer(new PongLayer());
     }
 };
 
 roo::Application* roo::CreateApplication()
 {
+    // Create a new application and send it to the engine
     return new MyApp();
 }
